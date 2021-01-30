3c112baf308758deba9efb423e02c7cd4f331069
兵頭洸介 <kosukehyodo0216@gmail.com>
2021-01-30 15:22:57 +0900

    fontの調整

--- assets/custom-theme.js
+++ assets/custom-theme.js
@@ -1,6 +1,6 @@
 (function () {
     var irori_section = document.querySelector(
-        "#shopify-section-16112562982c197b22 .custom-content"
+      "#shopify-section-16112562982c197b22 .custom-content .custom__item--image"
     );
 
     if (irori_section) {
